6f8b7128d02daa9f454ede01c378452344367c7f
Edcarlos Neves <edcarlosp@gmail.com>
2021-07-10 23:22:56 -0300

    Reformating code

--- project/apps/analyses/serializers.py
+++ project/apps/analyses/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 
 from project.apps.analyses.models import Analysis
 
+
 class AnalysisSerializer(serializers.ModelSerializer):
     class Meta:
         model = Analysis
-        fields = '__all__'
+        fields = "__all__"
--- project/apps/analyses/views.py
+++ project/apps/analyses/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from project.apps.analyses.models import Analysis
 from project.apps.analyses.serializers import AnalysisSerializer
 
+
 class AnalysisViewSet(viewsets.ModelViewSet):
     queryset = Analysis.objects.all().order_by("-id")
     serializer_class = AnalysisSerializer
