ce31879f3e871bca93295c8d5cd0baafd501f0fe
Edcalos Neves <edcarlosp@gmail.com>
2022-01-02 18:53:48 -0300

    inserting default values on variable - github actions

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,12 +68,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": config("DB_ENGINE"),
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
+        "ENGINE": config("DB_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": config("DB_NAME", default="calibrationcurve"),
+        "USER": config("DB_USER", default="calibrationcurve"),
+        "PASSWORD": config("DB_PASSWORD", default="calibrationcurve"),
+        "HOST": config("DB_HOST", default=None),
+        "PORT": config("DB_PORT", default=None),
     }
 }
 
